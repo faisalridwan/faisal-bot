547f28eef2926e5f9724dbd29c8ed5242901c24b
Faisal Ridwan  Siregar <faisalridwansiregar@student.telkomuniversity.ac.id>
2018-10-15 10:31:51 +0700

    ig

--- index.js
+++ index.js
@@ -276,6 +276,26 @@ function handleText(message, replyToken, source) {
         .then(() => client.leaveRoom(source.roomId));
       }
     }
+    else if (message.text.startsWith("ig: ")) {
+      var username = message.text.replace('ig: ','')
+      request({
+      url: 'https://www.instagram.com/' + username + '/?__a=1',
+      method: "GET",
+      headers: {
+        'Host': 'www.instagram.com',
+        'Cookie': process.env.INSTAGRAM_COOKIE,
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+      },
+      json: true
+    }, function (error, response, body){
+        if (body.graphql) {
+          var result = body.graphql.user;
+          let foto = result.profile_pic_url_hd;
+          return replyText(replyToken, 'foto :'+foto)
+
+        }
+      }
+    }
     else {
 
       console.log(`Echo message to ${replyToken}: ${message.text}`);
