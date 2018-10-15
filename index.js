27047adee8c06f571fad893176938146e6ed7d3a
Faisal Ridwan  Siregar <faisalridwansiregar@student.telkomuniversity.ac.id>
2018-10-15 10:36:55 +0700

    ig

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cp = require('child_process');
+const request = require('request'); //HTTP Request
 
 // create LINE SDK config from env variables
 const config = {
